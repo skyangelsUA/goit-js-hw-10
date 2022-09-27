d644e8796a63dee6821b94ca30d2d9e1b30f6b75
skyangelsUA <dkskyangels@gmail.com>
2022-09-27 04:13:23 +0300

    alpha html+JS+FETCH

---
+++ src/utils/fetch.js
@@ -0,0 +1,14 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+export function fetch(name) {
+    const url = `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`;
+
+
+    return fetch(url).then(r => {
+        if (!r.ok) {
+            Notify.failure(`На жаль, країни з такою назвою немає`);
+            throw Error(`Помилка: ${r.statusText}`);
+        }
+        return r.json()
+    });
+}
